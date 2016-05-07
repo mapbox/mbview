2ca89db53e3aa824075e3346001e1afd04bbaa99
Aaron Lidman <aaronlidman@gmail.com>
2016-05-06 22:41:03 -0700

    Make mbtiles parameter positional

--- cli.js
+++ cli.js
@@ -4,6 +4,7 @@ var argv = require('minimist')(process.argv.slice(2));
 var path = require('path');
 var open = require('open');
 
+argv.mbtiles = argv.mbtiles || argv._[0];
 if (!argv.mbtiles) {
     console.log(usage());
     process.exit(1);
@@ -13,10 +14,10 @@ argv.basemap = argv.basemap || argv.base || argv.map || 'dark';
 
 function usage () {
     var text = [];
-    text.push('usage: node cli.js [options]');
+    text.push('usage: mbview [file] [options]');
     text.push('');
-    text.push(' --mbtiles path to mbtiles file');
-    text.push(' --port sets port to use');
+    text.push(' --mbtiles path to mbtiles file (optional)');
+    text.push(' --port sets port to use (default: 3000)');
     text.push(' --quiet or -q supress all logging except the address to visit');
     text.push(' -n don\'t automatically open the browser on start');
     text.push(' --basemap or --base or --map sets the basemap style (default: dark)');

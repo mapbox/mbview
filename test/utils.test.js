c27458b9c20bca20c0fedbb04542dd681499fbc1
Rodolfo Wilhelmy <rwilhelmy@gmail.com>
2016-05-11 15:04:37 -0700

    Refactor out usage method - smaller cli.js interface
    Closes #27

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,6 +1,13 @@
 var test = require('tape').test;
 var utils = require('../utils');
 
+test('usage', function (t) {
+  var got = utils.usage();
+  t.true(got.match(/usage/), 'returns some instructions');
+  t.true(got.length > 100, 'lots of instructions');
+  t.end();
+});
+
 test('version', function (t) {
   var got = utils.version();
   t.true(got.match(/^\d+\.\d+\.\d+$/), 'finds basic semver in package.json');
--- utils.js
+++ utils.js
@@ -1,6 +1,24 @@
 var fs = require('fs');
 
 /**
+ * Get usage instructions
+ * @return {String} the instructions to run this thing
+ */
+module.exports.usage = function () {
+  var u = [];
+  u.push('usage: mbview [options] [files]');
+  u.push('');
+  u.push(' --port sets port to use (default: 3000)');
+  u.push(' --quiet or -q supress all logging except the address to visit');
+  u.push(' -n don\'t automatically open the browser on start');
+  u.push(' --basemap, --base or --map sets the basemap style (default: dark)');
+  u.push(' --version returns module version');
+  u.push(' --help prints this message');
+  u.push('');
+  return u.join('\n');
+};
+
+/**
  * Get module version from the package.json file
  * @return {String} version number
  */

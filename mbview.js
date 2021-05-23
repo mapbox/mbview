076cceae62143b34aefd25fb88d633aacea3799f
Matt Ficke <matt@mattficke.com>
2021-05-23 16:00:48 -0400

    Update mbview.js

--- mbview.js
+++ mbview.js
@@ -12,8 +12,8 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
